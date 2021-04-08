969219f0b7988f588d326ceb8deb4b93f0c67941
TheCARGOS <loayzavelez@gmail.com>
2021-04-08 10:36:02 -0500

    Styles were apllied to AdminPanel

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,9 +1,27 @@
 import "./AdminPanel.scss"
+import { Form, Input, TextArea, Button, Icon } from "semantic-ui-react"
 
 export default function AdminPanel() {
     return (
-        <div>
-            <h1>AdminPanel Component</h1>            
+        <div className="main-admin-panel">
+            <header className="main-admin-panel__header">
+                <h3 className="main-admin-panel__header__title">ADMIN PANEL</h3>
+            </header>
+            <Form className="main-admin-panel__form">
+                <Form.Field>
+                    <Input placeholder="To-Do name" />
+                </Form.Field>
+                <Form.Field>
+                    {/* <Input placeholder="To-Do name" /> */}
+                    <TextArea placeholder="Why would you do this
+to-do?"></TextArea>
+                </Form.Field>  
+
+                <Button fluid type="submit">
+                    <Icon name="plus" />
+                    ADD
+                </Button>
+            </Form>     
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.scss"
 export default function Header() {
     return (
         <div className="main-header">
-            <h1 className="main-header__title">To-Do List with ReactJS</h1>            
+            <h1 className="main-header__title">TODO APP WITH REACTJS</h1>            
         </div>
     )
 }
