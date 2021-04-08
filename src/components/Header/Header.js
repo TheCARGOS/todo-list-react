2d95da78d54c716fce84f80671f182364d4575ad
TheCARGOS <loayzavelez@gmail.com>
2021-04-08 09:49:23 -0500

    Header created and styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,8 @@ import "./Header.scss"
 
 export default function Header() {
     return (
-        <div>
-            <h1>Header Component</h1>            
+        <div className="main-header">
+            <h1 className="main-header__title">To-Do List with ReactJS</h1>            
         </div>
     )
 }
